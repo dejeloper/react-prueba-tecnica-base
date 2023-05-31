c5bd018d6e1de1a28ec658bae40ae136a8c6e7d9
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-05-31 13:49:56 -0500

    Add App Component and First Features

---
+++ App.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react'
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+const CAT_ENDPOINT_IMAGE = 'https://cataas.com/cat/says/'
+const CAT_PREFIX_IMAGE_URL = 'https://cataas.com/'
+
+export const App = () => {
+  const [fact, setFact] = useState()
+  const [threeWords, setThreeWords] = useState()
+  const [imageURL, setImageURL] = useState()
+
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then(res => res.json())
+      .then(data => {
+        const { fact } = data
+        setFact(fact)
+
+        setThreeWords(fact.split(' ').slice(0, 3).join(' '))
+
+        const endpoint = `${CAT_ENDPOINT_IMAGE}${threeWords}?size=50&color=black&json=true`
+
+        fetch(endpoint)
+          .then(res => res.json())
+          .then(response => {
+            const { url } = response
+            setImageURL(url)
+          })
+      })
+  }, [])
+
+  return (
+    <main>
+      <h1>Api de Gatos</h1>
+      {
+        fact && <p>{fact}</p>
+      }
+      {
+        imageURL &&
+        (
+          <picture>
+            <img src={`${CAT_PREFIX_IMAGE_URL}${imageURL}`} alt={`Imagen extrated using the first three words for ${fact}`} title={`Imagen extrated using the first three words for ${fact}`} />
+          </picture>
+        )
+      }
+    </main>
+  )
+}
