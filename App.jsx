6c32c20c59cffdb527edac52d274b10fb408899d
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-06-02 13:16:49 -0500

    Update useCatImage to link

--- App.jsx
+++ App.jsx
@@ -1,8 +1,6 @@
 import './App.css'
 import { useCatFact, useCatImage } from './hooks'
 
-const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
-
 export const App = () => {
   const { fact, getFact } = useCatFact()
   const { imageURL } = useCatImage({ fact })
@@ -21,7 +19,7 @@ export const App = () => {
           imageURL &&
           (
             <picture>
-              <img src={`${CAT_PREFIX_IMAGE_URL}${imageURL}`} alt={`Imagen extrated using the first three words for ${fact}`} title={`Imagen extrated using the first three words for ${fact}`} />
+              <img src={`${imageURL}`} alt={`Imagen extrated using the first three words for ${fact}`} title={`Imagen extrated using the first three words for ${fact}`} />
             </picture>
           )
         }
--- hooks/useCatImage.js
+++ hooks/useCatImage.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import { getImage } from '../services/facts'
 
+const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
+
 export const useCatImage = ({ fact, limit = 5 }) => {
   const [imageURL, setImageURL] = useState()
 
@@ -11,5 +13,5 @@ export const useCatImage = ({ fact, limit = 5 }) => {
     getImage(words).then(newUrl => setImageURL(newUrl))
   }, [fact])
 
-  return { imageURL }
+  return { imageURL: `${CAT_PREFIX_IMAGE_URL}${imageURL}` }
 }
