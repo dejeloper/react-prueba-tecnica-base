cb81181b10d769bd46b8f1a4e8680afa8cf4dbbc
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-06-02 13:00:18 -0500

    Add useCatImage Custom hook

--- App.jsx
+++ App.jsx
@@ -1,30 +1,21 @@
 import { useEffect, useState } from 'react'
-import { getImage, getRandomFact } from './services/facts'
+import { getRandomFact } from './services/facts'
+import { useCatImage } from './useCatImage'
 import './App.css'
 
 const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
 
 export const App = () => {
   const [fact, setFact] = useState()
-  const [imageURL, setImageURL] = useState()
+  const { imageURL } = useCatImage({ fact })
 
   const getFact = () => {
     getRandomFact().then(newFact => setFact(newFact))
   }
 
-  const getImg = () => {
-    if (!fact) return
-
-    getImage(fact).then(newUrl => setImageURL(newUrl))
-  }
-
   useEffect(() => {
     getFact()
   }, [])
-
-  useEffect(() => {
-    getImg()
-  }, [fact])
 
   const handlerClick = () => {
     getFact()
---
+++ useCatImage.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react'
+import { getImage } from './services/facts'
+
+export const useCatImage = ({ fact, limit = 5 }) => {
+  const [imageURL, setImageURL] = useState()
+
+  useEffect(() => {
+    if (!fact) return
+
+    const words = fact.split(' ').slice(0, limit).join(' ')
+    getImage(words).then(newUrl => setImageURL(newUrl))
+  }, [fact])
+
+  return { imageURL }
+}
