fd6792a5569a8362ea364f1ff3a4267484c7920c
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-06-02 12:49:06 -0500

    Add new Services for get Image

--- App.jsx
+++ App.jsx
@@ -1,8 +1,7 @@
 import { useEffect, useState } from 'react'
+import { getImage, getRandomFact } from './services/facts'
 import './App.css'
-import { getRandomFact } from './services/facts'
 
-const CAT_ENDPOINT_IMAGE = 'https://cataas.com/cat/says/'
 const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
 
 export const App = () => {
@@ -13,22 +12,18 @@ export const App = () => {
     getRandomFact().then(newFact => setFact(newFact))
   }
 
+  const getImg = () => {
+    if (!fact) return
+
+    getImage(fact).then(newUrl => setImageURL(newUrl))
+  }
+
   useEffect(() => {
     getFact()
   }, [])
 
   useEffect(() => {
-    if (!fact) return
-
-    const threeWords = fact.split(' ').slice(0, 3).join(' ')
-    const endpoint = `${CAT_ENDPOINT_IMAGE}${threeWords}?size=50&color=black&json=true`
-
-    fetch(endpoint)
-      .then(res => res.json())
-      .then(response => {
-        const { url } = response
-        setImageURL(url)
-      })
+    getImg()
   }, [fact])
 
   const handlerClick = () => {
--- services/facts.js
+++ services/facts.js
@@ -1,4 +1,5 @@
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+const CAT_ENDPOINT_IMAGE = 'https://cataas.com/cat/says/'
 
 export const getRandomFact = () => {
   return fetch(CAT_ENDPOINT_RANDOM_FACT)
@@ -6,5 +7,16 @@ export const getRandomFact = () => {
     .then(data => {
       const { fact } = data
       return fact
+    })
+}
+
+export const getImage = (filter) => {
+  const endpoint = `${CAT_ENDPOINT_IMAGE}${filter}?size=50&color=black&json=true`
+  console.log(endpoint)
+  return fetch(endpoint)
+    .then(res => res.json())
+    .then(response => {
+      const { url } = response
+      return url
     })
 }
