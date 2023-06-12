6895009dcd9f44d40adbdec30fcf4306362e761f
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-06-12 12:16:12 -0500

    Update testing for demo

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -4,15 +4,15 @@ import { test, expect } from '@playwright/test'
 const LOCALHOST_URL = 'http://localhost:5173/'
 const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
 
-test('app has title', async ({ page }) => {
-  // await page.waitForTimeout(0)
-  await page.goto(LOCALHOST_URL)
+// test('app has title', async ({ page }) => {
+//   // await page.waitForTimeout(0)
+//   await page.goto(LOCALHOST_URL)
 
-  const h1 = await page.getByRole('heading')
-  const h1Text = await h1.textContent()
+//   const h1 = await page.getByRole('heading')
+//   const h1Text = await h1.textContent()
 
-  await expect(h1Text?.length).toBeGreaterThan(0)
-})
+//   await expect(h1Text?.length).toBeGreaterThan(0)
+// })
 
 test('app has success title', async ({ page }) => {
   // await page.waitForTimeout(0)
@@ -30,13 +30,13 @@ test('app get a fact', async ({ page }) => {
   const parrafo = await page.getByRole('paragraph')
   const fact = await parrafo.textContent()
 
-  await expect(fact?.length).toBeGreaterThan(0)
-})
-
-test('app render a image', async ({ page }) => {
-  await page.goto(LOCALHOST_URL)
   const image = await page.getByRole('img')
   const imgSrc = await image.getAttribute('src')
 
+  await expect(fact?.length).toBeGreaterThan(0)
   await expect(imgSrc?.startsWith(CAT_PREFIX_IMAGE_URL)).toBeTruthy()
 })
+
+// test('app changes <p> and <img> for click on the button', async ({ page }) => {
+//   await page.goto(LOCALHOST_URL)
+// })
